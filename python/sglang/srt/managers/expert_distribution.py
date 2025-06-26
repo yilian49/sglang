8a419ce6e0e638f477360b9e6b7f30afee2148fa
yilian49 <bliu0530@gmail.com>
2025-06-26 21:47:42 +0000

    assert fix

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -479,9 +479,8 @@ class _SelectExpertsSinglePassGatherer(_LayerBasedGpuSinglePassGatherer):
     def on_select_experts(self, layer_idx: int, topk_ids: torch.Tensor):
         topk_ids = topk_ids.flatten()
         mask = topk_ids != -1
-        assert self._data[layer_idx, :].shape == topk_ids.shape, (
-            "Shape mismatch between data and topk_ids."
-            "Selecting expert is not supported for multiple token prediction at the moment."
+        assert self._data[layer_idx, :].ndim == topk_ids.ndim, (
+            "rank mismatch between layer data and topk_ids"
         )
         self._data[layer_idx, :].scatter_add_(
             dim=0, index=topk_ids.masked_fill(~mask, 0).long(), src=mask.int()
